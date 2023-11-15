3fabc5c96d1900ca074e6671c2308dd3be9583a7
Amjadyabroudi128 <amjadyabroudi.20@gmail.com>
2023-11-15 00:00:56 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# php-with-flutter" 
+"# flutterphp" 
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+ $price = 1200;
+ if($price > 1000) {
+ echo "price is less than 2000";
+ } else {
+ echo "what is up gamers ";
+ }
+?>
